eacd348fc5081a20babb89726b0cc5c28a720dd4
Nilo Martinez Jr <nilofeliu@hotmail.com>
2025-09-09 08:57:26 -0400

    final: clean up and finalize project

--- element.sh
+++ element.sh
@@ -1,2 +1,22 @@
-[[ $# -eq 0 ]] && echo "Please provide an element as an argument."
-# logic to query database goes here
+#!/bin/bash
+
+PSQL="psql --username=postgres --dbname=periodic_table -t --no-align -c"
+
+# Check if argument is provided
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+# Query the database
+ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number::text='$1' OR symbol='$1' OR name='$1'")
+
+# Check if element was found
+if [[ -z $ELEMENT_INFO ]]; then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+# Parse and format output
+IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT_INFO"
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
